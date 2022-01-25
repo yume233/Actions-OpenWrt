d04bda28a9d5b56ee1b7ee5b5ef2cca38960aa02
Umia <akiayumei@gmail.com>
2022-01-25 13:19:31 +0800

    _(:з」∠)_

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone https://github.com/BROBIRD/openwrt-r8168 package/openwrt-r8168
