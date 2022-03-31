7aa6e391af9c83c55f36f6aa00931c9b4aed9032
yume233 <akiayumei@gmail.com>
2022-03-31 22:39:18 +0800

    我是笨比

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
-# echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
