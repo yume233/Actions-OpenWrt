d5dd26d6654c96f10303cd549b73b705f5cc804e
yume233 <akiayumei@gmail.com>
2022-03-31 13:05:43 +0800

    ✨ 再来!

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/BROBIRD/openwrt-r8168 package/openwrt-r8168
