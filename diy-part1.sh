0a3e57e606028e98abfb1ba2fe106391273b882f
Umia <akiayumei@gmail.com>
2022-03-31 13:55:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
