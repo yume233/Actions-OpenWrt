3f8269baf81b4ec0aac082faaa0a1510397e4514
Umia <akiayumei@gmail.com>
2024-07-31 01:43:10 +0800

    呜呜

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@ echo "src-git dead https://github.com/QiuSimons/luci-app-daed" >> feeds.conf.def
 echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/allegro0132/Openwrt-mlnx-ofed.git package/
